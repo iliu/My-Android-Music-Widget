2b9cbb7cf398c4a56325b4ab214d7203bc122048
Isaac Liu <isaacliu@isaacmac.gateway.2wire.net>
2010-09-03 19:18:36 -0700

    added some buttons and played around with the layout of the widget

--- gen/com/sacliv/simpmusicwidget/R.java
+++ gen/com/sacliv/simpmusicwidget/R.java
@@ -13,12 +13,22 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int artist=0x7f060001;
+        public static final int next_button=0x7f060003;
+        public static final int play_button=0x7f060002;
+        public static final int title=0x7f060000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
+        public static final int artist=0x7f050005;
         public static final int hello=0x7f050000;
+        public static final int next=0x7f050003;
+        public static final int play=0x7f050002;
+        public static final int title=0x7f050004;
     }
     public static final class xml {
         public static final int simpmusic_widget_provider=0x7f040000;
